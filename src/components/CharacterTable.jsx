b5f262d9e271746c32d831dfdffb9f782404cc98
Reinhard Wansch <reinhard@wansch.at>
2026-02-20 20:16:52 +0100

    fix backgrond-color issue on char buttons, tailwind-doc: "Don't construct class names dynamically"

--- src/components/CharacterTable.jsx
+++ src/components/CharacterTable.jsx
@@ -4,11 +4,16 @@ export default function CharacterTable({ characters, handleClick, isGameOver, is
     const styleBase = "h-[2.5rem] w-[2.5rem] border border-char-border flex justify-center items-center";
     const styleDisabled = isDisabled ? " opacity-20" : " cursor-pointer";
     const style = styleBase + styleDisabled;
+    const bgStyles= {
+        open: " bg-char-open",
+        hit: " bg-char-hit",
+        fail: " bg-char-fail"
+    }
 
     return (
         <div className="w-[480px] flex justify-center gap-2 flex-wrap">
             {characters.map(charI => {
-                const styleBg= " bg-char-" + charI.status;
+                const styleBg= bgStyles[charI.status];
                 return <button
                     className={style + styleBg}
                     key={charI.id}
