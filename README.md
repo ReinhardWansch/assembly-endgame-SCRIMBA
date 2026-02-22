a9f3d782f811c5a1d2360ccbbfabaf166e795b61
Reinhard Wansch <reinhard@wansch.at>
2026-02-22 03:30:50 +0100

    farewell messages

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+# Assembly Endgame
 
-# TODO
+A project from the course [Learn React](https://scrimba.com/learn-react-c0e:toc) by Scrimba.
 
-- Merge origin/main with main from work
-- Favicon!
+[Intro lesson](https://scrimba.com/learn-react-c0e/~0y62)  
+[Figma file](https://www.figma.com/design/VJNO8MeMT3E0B2twQ1HajU/Assembly--Endgame?node-id=0-1&p=f&t=6X2PTEVJY0CpV6Ir-0)
+
+Check **branch "reini"** for my solution without tutorial  
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import { useState } from "react"
 import { clsx } from "clsx"
 import { languages } from "./languages"
-
-/**
- * Goal: Add in the incorrect guesses mechanism to the game
- * 
- * Challenge:
- * Conditionally render either the "won" or "lost" statuses
- * from the design, both the text and the styles, based on the
- * new derived variables.
- * 
- * Note: We always want the surrounding `section` to be rendered,
- * so only change the content inside that section. Otherwise the
- * content on the page would jump around a bit too much.
- */
+import { getFarewellText } from "./utils"
 
 export default function AssemblyEndgame() {
     // State values
@@ -27,6 +15,8 @@ export default function AssemblyEndgame() {
         currentWord.split("").every(letter => guessedLetters.includes(letter))
     const isGameLost = wrongGuessCount >= languages.length - 1
     const isGameOver = isGameWon || isGameLost
+    const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
+    const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
 
     // Static values
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -85,34 +75,49 @@ export default function AssemblyEndgame() {
 
     const gameStatusClass = clsx("game-status", {
         won: isGameWon,
-        lost: isGameLost
+        lost: isGameLost,
+        farewell: !isGameOver && isLastGuessIncorrect
     })
+
+    function renderGameStatus() {
+        if (!isGameOver && isLastGuessIncorrect) {
+            return (
+                <p className="farewell-message">
+                    {getFarewellText(languages[wrongGuessCount - 1].name)}
+                </p>
+            )
+        }
+
+        if (isGameWon) {
+            return (
+                <>
+                    <h2>You win!</h2>
+                    <p>Well done! 🎉</p>
+                </>
+            )
+        } 
+        if (isGameLost) {
+            return (
+                <>
+                    <h2>Game over!</h2>
+                    <p>You lose! Better start learning Assembly 😭</p>
+                </>
+            )
+        }
+        
+        return null
+    }
 
     return (
         <main>
             <header>
                 <h1>Assembly: Endgame</h1>
                 <p>Guess the word within 8 attempts to keep the
-                    programming world safe from Assembly!</p>
+                programming world safe from Assembly!</p>
             </header>
 
             <section className={gameStatusClass}>
-                {isGameOver ? (
-                    isGameWon ? (
-                        <>
-                            <h2>You win!</h2>
-                            <p>Well done! 🎉</p>
-                        </>
-                    ) : (
-                        <>
-                            <h2>Game over!</h2>
-                            <p>You lose! Better start learning Assembly 😭</p>
-                        </>
-                    )
-                ) : (
-                    null
-                )
-                }
+                {renderGameStatus()}
             </section>
 
             <section className="language-chips">
