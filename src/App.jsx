88975c5895194b87ff2bae764f78d790415a41ca
Reinhard Wansch <reinhard@wansch.at>
2026-02-22 08:07:35 +0100

    display missed letters when lost

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ import { getFarewellText, getRandomWord } from "./utils"
  * ✅ Fix a11y issues
  * ✅ Choose a random word from a list of words
  * ✅ Make the New Game button reset the game
- * - Reveal what the word was if the user loses the game
+ * ✅ Reveal what the word was if the user loses the game
  * - Confetti drop when the user wins
  * 
- * Challenge: Make the New Game button reset the game
+ * Challenge: Reveal the missing letters of the word if the user
+ * loses the game. Style the missing letters to have the same red
+ * color as the wrong letter keys.
  */
 
 export default function AssemblyEndgame() {
@@ -67,11 +69,17 @@ export default function AssemblyEndgame() {
         )
     })
 
-    const letterElements = currentWord.split("").map((letter, index) => (
-        <span key={index}>
-            {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
-        </span>
-    ))
+    const letterElements = currentWord.split("").map((letter, index) => {
+        const shouldRevealLetter = isGameLost || guessedLetters.includes(letter)
+        const letterClassName = clsx(
+            isGameLost && !guessedLetters.includes(letter) && "missed-letter"
+        )
+        return (
+            <span key={index} className={letterClassName}>
+                {shouldRevealLetter ? letter.toUpperCase() : ""}
+            </span>
+        )
+    })
 
     const keyboardElements = alphabet.split("").map(letter => {
         const isGuessed = guessedLetters.includes(letter)
